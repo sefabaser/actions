9840f1282829e7bc07b89a85f938df7f2d743925
sefa baser <sefabaser84@hotmail.com>
2019-08-08 15:05:30 +0200

    angular decorators

---
+++ src/helpers/angular-decorators.ts
@@ -0,0 +1,62 @@
+import { Comparator } from 'helpers-lib';
+
+import { ActionSubscription } from './notification-handler';
+import { Notifier } from '../notifier/notifier';
+import { Reducer } from '../reducer/reducer';
+
+/**
+ * IMPORTANT: In case of AOT build, components should define 'ngOnInit' and 'ngOnDestroy' even tough they are empty
+ * AOT build is stripping decorator defined functions, therefore angular cannot trigger decorator defined ngOnInit and ngOnDestroy functions
+ */
+export function SubscribeAction(action: Notifier<any> | Reducer<any, any>) {
+  return function(constructor: any, key: string) {
+    let subscription: ActionSubscription;
+
+    let initCallback = constructor.ngOnInit;
+    let destroyCallback = constructor.ngOnDestroy;
+    if (!Comparator.isFunction(initCallback)) {
+      console.error(`You cannot use 'SubscribeAction' without ngOnInit! "${key}"`);
+    }
+    if (!Comparator.isFunction(destroyCallback)) {
+      console.error(`You cannot use 'SubscribeAction' without ngOnDestroy! "${key}"`);
+    }
+
+    constructor.ngOnInit = function() {
+      if (action instanceof Notifier) {
+        subscription = action.subscribe(item => {
+          this[key] = item;
+        });
+      } else {
+        subscription = action.subscribe(item => {
+          this[key] = item;
+        });
+      }
+
+      Comparator.isFunction(initCallback) && initCallback.bind(this)();
+    };
+
+    constructor.ngOnDestroy = function() {
+      Comparator.isFunction(destroyCallback) && destroyCallback.bind(this)();
+      subscription && subscription.unsubscribe();
+    };
+  };
+}
+
+/**
+ * IMPORTANT: In case of AOT build, components should define 'ngOnDestroy' even tough it is empty
+ * AOT build is stripping decorator defined functions, therefore angular cannot trigger decorator defined ngOnInit and ngOnDestroy functions
+ */
+export function AutoSubscription() {
+  return function(constructor: any, key: string) {
+    let destroyCallback = constructor.ngOnDestroy;
+    if (!Comparator.isFunction(destroyCallback)) {
+      console.error(`You cannot use 'AutoSubscription' without ngOnDestroy! "${key}"`);
+    }
+
+    constructor.ngOnDestroy = function() {
+      destroyCallback && destroyCallback.bind(this)();
+      let subscription = this[key];
+      subscription && subscription.unsubscribe();
+    };
+  };
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { SubscribeAction, AutoSubscription } from './helpers/angular-decorators';
 export { ActionSubscription } from './helpers/notification-handler';
 export { Notifier, NotifierOptions, NotifierListenerCallbackFunction } from './notifier/notifier';
 export { Reducer, ReducerEffectChannel, ReducerReduceFunction } from './reducer/reducer';
