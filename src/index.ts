e6eaba7d593bf256fc00157adef4d6b1fade50fe
sefabaser <sefabaser84@hotmail.com>
2024-04-20 07:09:19 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { ActionLibDefaults } from './config';
 export { ActionSubscription } from './helpers/notification-handler';
 export { Action, ActionOptions, ActionListenerCallbackFunction } from './app/action/action';
-export { Variable, VariableOptions, VariableListenerCallbackFunction } from './app/variable/variable';
+export { Variable, VariableOptions, VariableListenerCallbackFunction, IVariable } from './app/variable/variable';
 export { Reducer, ReducerEffectChannel, ReducerReduceFunction } from './app/reducer/reducer';
 export { ObservableMap } from './app/observable-map/observable-map';
 export { ObservableSet } from './app/observable-set/observable-set';
