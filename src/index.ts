e483136cfd738e8e9beef1d31723832ba3e1f6a4
sefabaser <sefabaser84@hotmail.com>
2025-10-30 17:12:18 +0100

    minor

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { Attachable } from './attachable/attachable';
-export { IAttachable } from './attachable/base-attachable';
+export { BaseAttachable, IAttachable } from './attachable/base-attachable';
 export { ClassID } from './attachable/helpers/class-id';
 export { IDAttachable } from './attachable/id-attachable';
 export { ActionLibDefaults } from './config';
