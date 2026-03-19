0ddc39ca8333e365556abc80280e2f0548749044
sefabaser <sefabaser84@hotmail.com>
2026-03-19 13:03:50 +0100

    test setup

--- src/observables/process/process.test.ts
+++ src/observables/process/process.test.ts
@@ -1,3 +1,7 @@
+import { beforeEach, describe, expect, test } from 'vitest';
+
+import { Process } from './process';
+
 // no registerers on start
 // all registerers instantly resolves
 
@@ -8,3 +12,17 @@
 
 // destroying the process should destroy all listener processes
 // destroying the process after one child listener destroyed should destroy the remaining children
+
+describe('Process', () => {
+  describe('basic behavior', () => {
+    let process: Process<number, number, number>;
+
+    beforeEach(() => {
+      process = new Process<number, number, number>((acc, value) => acc + value, 0);
+    });
+
+    test('defined', () => {
+      expect(process).toBeDefined();
+    });
+  });
+});
